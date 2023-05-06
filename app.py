bcc9de4ed8443bcaa96600d4f524da32a50fad72
CD777XC <cd777xc@proton.me>
2023-05-06 23:47:43 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/exp")
+def exp():
+    return render_template("exp.html")
